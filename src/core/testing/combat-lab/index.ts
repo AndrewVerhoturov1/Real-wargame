7256ac2d8579bf160de6982545e95dfb681a42da
AndrewVerhoturov1 <andrew.verhoturov@mail.ru>
2026-07-26 13:12:59 +0500

    feat: export Combat Lab core

---
+++ src/core/testing/combat-lab/index.ts
@@ -0,0 +1,6 @@
+export * from './CombatLabContracts';
+export * from './CombatLabCommands';
+export * from './CombatLabDigest';
+export * from './CombatLabMetrics';
+export * from './CombatLabRunner';
+export * from './CombatLabScenarioRegistry';
