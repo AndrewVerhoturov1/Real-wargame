df0c9e8b6ac3fd88c6ad64e8571283bf996ec0c3
AndrewVerhoturov1 <andrew.verhoturov@mail.ru>
2026-08-16 03:26:35 +0500

    feat: export series analysis

--- src/core/testing/combat-lab/experiment/index.ts
+++ src/core/testing/combat-lab/experiment/index.ts
@@ -14,3 +14,4 @@ export * from './CombatLabBatchStatistics';
 export * from './CombatLabRepresentativeRuns';
 export * from './CombatLabBatchRunner';
 export * from './CombatLabSeriesRecords';
+export * from './CombatLabSeriesAnalysis';
